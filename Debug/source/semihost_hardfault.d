1127fa324abd39ce2302a9ababc300af7495588b
Ali Hassan <alihassan.pk019@gmail.com>
2025-06-20 12:02:30 +0500

    original sdk

--- Debug/source/semihost_hardfault.d
+++
@@ -1,4 +0,0 @@
-source/semihost_hardfault.o source/semihost_hardfault.d: \
- ../source/semihost_hardfault.c \
- C:\Users\Artak\Documents\MCUXpressoIDE_24.12.148\workspace\USB\ Dongle_dev_audio_generator_lite_bm\source\mcux_config.h
-C:\Users\Artak\Documents\MCUXpressoIDE_24.12.148\workspace\USB\ Dongle_dev_audio_generator_lite_bm\source\mcux_config.h:
